2fd6076c0a7354c356a9fb1915a39da94feb6ed1
sadmanador <84454116+sadmanador@users.noreply.github.com>
2022-08-16 20:13:24 +0600

    string method done till trim()

--- app/script.js
+++ app/script.js
@@ -62,4 +62,39 @@ const poem3 = "true\\false";
 document.getElementById("demo7").innerHTML = poem1 + "<br>" + poem2 + "<br>" + poem3;
 
 
-//
+//string line break
+document.getElementById("demo8").innerHTML = "This is a long line \
+of of string.";
+
+//string into object
+let bmw = "car";
+let toyota = new String("car");
+
+document.getElementById("demo9").innerHTML = typeof bmw + "<br>" + typeof toyota;
+
+
+
+
+//string method
+//string slice
+let fruitBasket = "Apple, Banana, Kiwi";
+document.getElementById("demo10").innerHTML = fruitBasket.slice(7, 13);
+
+//string substring
+document.getElementById("demo11").innerHTML = fruitBasket.substring(-3, 5);
+
+//substr
+document.getElementById("demo12").innerHTML = fruitBasket.substr(15, 18);
+
+//string.replace
+let billionaire = "The top BILLIONAIRE is the founder of Microsoft.";
+let newBillionaire = billionaire.replace("Microsoft", "Tesla");
+document.getElementById("demo13").innerHTML = newBillionaire;
+
+document.getElementById("demo14").innerHTML = billionaire.replace(/billionaire/i, "MILLIONARE");
+
+let million = "I told told you million times that earning million dollars its not joke. one need to invest his millions of hours to become a true million dollar guy.";
+document.getElementById("demo15").innerHTML = million.replace(/million/g, "thousand");
+
+let excessStr = "                                 Hello World!";
+document.getElementById("demo16").innerHTML = excessStr.trim();
