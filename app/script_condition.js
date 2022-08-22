6af1b488a1974fb556c864e93409f87145bcc005
sadmanador <84454116+sadmanador@users.noreply.github.com>
2022-08-22 21:22:38 +0600

    for loop done

--- app/script_condition.js
+++ app/script_condition.js
@@ -16,3 +16,18 @@ if (isRaining){
 let is100 = 100;
 let is99 = 99;
 document.getElementById("booleanFunc").innerHTML = Boolean(is100 > is99);
+
+
+
+//if statement
+let scoreMark = 82;
+if (scoreMark >= 60 && scoreMark < 80){
+    document.getElementById("ifStatement").innerHTML = "You passed, But have to work more";
+} else if (scoreMark >= 80 && scoreMark <= 100){
+    document.getElementById("ifStatement").innerHTML = "Well Done!";
+} else {
+    document.getElementById("ifStatement").innerHTML = "You failed";
+}
+
+
+//switch
